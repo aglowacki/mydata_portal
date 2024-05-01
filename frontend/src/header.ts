94c7b00f56e6290cb5efe2ab859c6dd71b70456f
Arthur Glowacki <aglowakci@anl.gov>
2024-05-01 11:50:26 -0500

    update css and started adding nav

--- frontend/src/header.ts
+++ frontend/src/header.ts
@@ -3,7 +3,18 @@ export function gen_header()
     const header = document.createElement("header");
     header.classList.add("header");
     
-    
+    const header_nav = document.createElement("nav");
+    header_nav.classList.add("nav");
+
+    const home_ul = document.createElement("ul");
+    home_ul.innerHTML = "Home";
+    home_ul.classList.add("nav");
+    home_ul.classList.add("ul");
+
+    header_nav.appendChild(home_ul);
+
+
+    header.appendChild(header_nav);
     return header;
 }
 
