f35cf216a835eb61b9be8251090ed27a229c2176
aglowacki <aglowacki.anl@gmail.com>
2024-05-01 20:05:40 +0000

    started adding other links (datasets, beamline)

--- frontend/src/header.ts
+++ frontend/src/header.ts
@@ -1,3 +1,25 @@
+function gen_beam_time_link()
+{
+    // if logged in and user has beam time
+    const beam_a = document.createElement("a");
+    beam_a.classList.add("nav");
+    beam_a.href = "/beamline";
+    beam_a.innerHTML = "Beamline";
+
+    return beam_a;
+}
+
+function gen_datasets_link()
+{
+    // if logged in and user has beam time
+    const datasets_a = document.createElement("a");
+    datasets_a.classList.add("nav");
+    datasets_a.href = "/datasets";
+    datasets_a.innerHTML = "Datasets";
+
+    return datasets_a;
+}
+
 function gen_signin_form()
 {
     const form = document.createElement("form");
@@ -41,6 +63,23 @@ function gen_signin_form()
     return form;
 }
 
+function gen_user_icon()
+{
+    
+/*
+ <a href="#" class="desktop-item">Dropdown Menu</a>
+        <input type="checkbox" id="showDrop">
+        <label for="showDrop" class="mobile-item">Dropdown Menu</label>
+        <ul class="drop-menu">
+          <li><a href="#">Drop menu 1</a></li>
+          <li><a href="#">Drop menu 2</a></li>
+          <li><a href="#">Drop menu 3</a></li>
+          <li><a href="#">Drop menu 4</a></li>
+        </ul>
+*/
+
+}
+
 export function gen_header()
 {
     const header = document.createElement("header");
@@ -50,19 +89,16 @@ export function gen_header()
     const header_nav = document.createElement("nav");
     header_nav.classList.add("nav");
 
-    //const nav_ul = document.createElement("ul");
-    //nav_ul.classList.add("nav");
-    //nav_ul.classList.add("ul");
-
+    // always show home link in header
     const home_a = document.createElement("a");
     home_a.classList.add("nav");
     home_a.href = "/";
     home_a.innerHTML = "Home";
-    
 
     header.appendChild(header_nav);
-    //header_nav.appendChild(nav_ul);
     header_nav.appendChild(home_a);
+    header_nav.appendChild(gen_beam_time_link());
+    header_nav.appendChild(gen_datasets_link());
     header_nav.appendChild(gen_signin_form());
 
     return header;
--- frontend/src/left-sidebar.ts
+++ frontend/src/left-sidebar.ts
@@ -1,3 +1,13 @@
+export function gen_datasets_left_sidebar()
+{
+    const main_div = document.createElement("div");
+    main_div.classList.add("left-sidebar");
+    
+    
+    return main_div;
+}
+
+
 export function gen_left_sidebar()
 {
     const main_div = document.createElement("div");
