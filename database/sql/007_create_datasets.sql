560549a61bc25f3dabec7f15edc8fc75aa786851
Arthur Glowacki <aglowakci@anl.gov>
2025-02-27 09:52:42 -0600

    Change datetime to timestamp

--- database/sql/007_create_datasets.sql
+++ database/sql/007_create_datasets.sql
@@ -4,5 +4,5 @@ beamline_id integer REFERENCES Beamlines (id),
 run_id integer REFERENCES SyncotronRuns (id),
 scan_type_id REFERENCES ScanType (id),
 datastore_id REFERENCES DataStore (id),
-AcquisitionDateTime DATETIME NOT NULL
+AcquisitionDateTime TIMESTAMP NOT NULL
 );
--- database/sql/010_create_analysis.sql
+++ database/sql/010_create_analysis.sql
@@ -2,7 +2,7 @@ CREATE TABLE Analysis (
 id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 dataset_id integer REFERENCES Datasets (id),
 datastore_id REFERENCES DataStore (id),
-AnalysisSubmitTime DATETIME NOT NULL,
-ProcessingStartTime DATETIME,
-ProcessingEndTime DATETIME
+AnalysisSubmitTime TIMESTAMP NOT NULL,
+ProcessingStartTime TIMESTAMP,
+ProcessingEndTime TIMESTAMP
 );
