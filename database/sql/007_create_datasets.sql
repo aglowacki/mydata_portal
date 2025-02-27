748ae72f74a5b49b20b80fc765f7f3af70d1ede7
Arthur Glowacki <aglowakci@anl.gov>
2025-02-27 10:10:08 -0600

    Fix sql create

--- database/sql/007_create_datasets.sql
+++ database/sql/007_create_datasets.sql
@@ -2,7 +2,7 @@ CREATE TABLE "Datasets" (
 id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 beamline_id integer REFERENCES "Beamlines" (id),
 run_id integer REFERENCES "SyncotronRuns" (id),
-scan_type_id REFERENCES "ScanType" (id),
-datastore_id REFERENCES "DataStore" (id),
+scan_type_id integer REFERENCES "ScanType" (id),
+datastore_id integer REFERENCES "DataStore" (id),
 AcquisitionDateTime TIMESTAMP NOT NULL
 );
