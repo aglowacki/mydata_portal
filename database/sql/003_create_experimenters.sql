d44e09f6e56265e9713d0688eb16d6c073079a2b
Arthur Glowacki <aglowakci@anl.gov>
2025-05-27 18:00:55 -0500

    fixed typo

--- database/sql/003_create_experimenters.sql
+++ database/sql/003_create_experimenters.sql
@@ -1,5 +1,5 @@
 CREATE TABLE experimenters (
-datasest_id integer REFERENCES datasets (id),
+dataset_id integer REFERENCES datasets (id),
 user_badge integer REFERENCES users (badge),
 proposal_id integer REFERENCES proposals (id),
 experiment_role_id integer REFERENCES experiment_roles (id)
