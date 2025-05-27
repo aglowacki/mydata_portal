cd1d12ba1af50ac65b0f350ec88d89c485818c45
Arthur Glowacki <aglowakci@anl.gov>
2025-05-27 16:51:51 -0500

    Added insert sync run script

--- database/sql/all_insert.sh
+++ database/sql/all_insert.sh
@@ -6,3 +6,4 @@ psql -d mydata -f 102_insert_users_staff.sql
 psql -d mydata -f 103_insert_beamline_contacts.sql
 psql -d mydata -f 104_insert_experiment_roles.sql
 psql -d mydata -f 105_insert_scan_types.sql
+psql -d mydata -f 106_insert_sync_runs.sql
