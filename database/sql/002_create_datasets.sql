ca98126fd3f2d8aee96d4003159c3f94257ed740
Arthur Glowacki <aglowakci@anl.gov>
2025-05-27 17:51:19 -0500

    Removed data_store from analysis

--- database/sql/002_create_datasets.sql
+++ database/sql/002_create_datasets.sql
@@ -1,6 +1,6 @@
 CREATE TABLE datasets (
 id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-path varchar(2000),
+path varchar(2000) NOT NULL,
 acquisition_timestamp TIMESTAMP NOT NULL,
 beamline_id integer REFERENCES beamlines (id),
 syncotron_run_id integer REFERENCES syncotron_runs (id),
