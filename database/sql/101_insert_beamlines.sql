5f6ed38266b82a015370d185dacc35c7d1265b44
Arthur Glowacki <aglowakci@anl.gov>
2025-05-27 16:54:04 -0500

    Made beamline acry upper case

--- database/sql/101_insert_beamlines.sql
+++ database/sql/101_insert_beamlines.sql
@@ -1,9 +1,9 @@
 INSERT INTO beamlines (acronym, name, division, link) VALUES 
-('2-id-d', 'Microfluorescence', 'Microscopy', 'https://bionanoprobe.xray.aps.anl.gov/'),
+('2-ID-D', 'Microfluorescence', 'Microscopy', 'https://bionanoprobe.xray.aps.anl.gov/'),
 ('BNP', 'Microfluorescence and Bionanoprobe', 'Microscopy', 'https://bionanoprobe.xray.aps.anl.gov/'),
-('2-id-e', 'Microprobe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=61'),
-('8-bm', 'X-ray Microscopy', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=33'),
-('19-id-e', 'In-situ Nanoprobe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=232'),
-('26-id-c', 'Hard X-ray Nanoprobe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=92'),
-('33-id-c', 'PtychoProbe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=241')
+('2-ID-E', 'Microprobe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=61'),
+('8-BM', 'X-ray Microscopy', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=33'),
+('19-ID-E', 'In-situ Nanoprobe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=232'),
+('26-ID-C', 'Hard X-ray Nanoprobe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=92'),
+('33-ID-C', 'PtychoProbe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=241')
 ;
