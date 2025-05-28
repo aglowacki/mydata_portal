caf6d3970840f1869099cfb85576c98e3d598288
Arthur Glowacki <aglowakci@anl.gov>
2025-05-28 10:10:55 -0500

    update to have val for old_acronym

--- database/sql/101_insert_beamlines.sql
+++ database/sql/101_insert_beamlines.sql
@@ -1,11 +1,9 @@
-INSERT INTO beamlines (acronym, name, division, link) VALUES 
-('2-ID-D', 'Microfluorescence', 'Microscopy', 'https://bionanoprobe.xray.aps.anl.gov/'),
-('2-ID-E', 'Microprobe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=61'),
-('19-ID-E', 'In-situ Nanoprobe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=232'),
-('26-ID-C', 'Hard X-ray Nanoprobe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=92'),
-('33-ID-C', 'PtychoProbe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=241')
-;
 
 INSERT INTO beamlines (acronym, old_acronym, name, division, link) VALUES 
 ('BNP', '9-ID-B,C', 'Microfluorescence and Bionanoprobe', 'Microscopy', 'https://bionanoprobe.xray.aps.anl.gov/'),
-('8-BM-B', '8-BM', 'X-ray Microscopy', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=33');
+('8-BM-B', '8-BM', 'X-ray Microscopy', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=33'),
+('2-ID-D', ' ', 'Microfluorescence', 'Microscopy', 'https://bionanoprobe.xray.aps.anl.gov/'),
+('2-ID-E', ' ', 'Microprobe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=61'),
+('19-ID-E', ' ','In-situ Nanoprobe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=232'),
+('26-ID-C', ' ', 'Hard X-ray Nanoprobe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=92'),
+('33-ID-C', ' ', 'PtychoProbe', 'Microscopy', 'https://aps.anl.gov/Beamlines/Directory/Details?beamline_id=241');
