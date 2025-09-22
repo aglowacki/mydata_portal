7ff7e559fd3535420defd6305e0d0881be1c8b1e
Arthur Glowacki <aglowakci@anl.gov>
2025-09-22 11:07:08 -0500

    Added zmq bs qs worker to read from list of qs clients and insert messaged to redis

--- Worker/src/config.rs
+++ Worker/src/config.rs
@@ -1,20 +1,23 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Config {
-    pub redis_config: Redis_Config,
-    pub bluesky_clients: Vec<BS_Client>
+pub struct Config
+{
+    pub redis_config: RedisConfig,
+    pub bluesky_clients: Vec<BsClient>
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Redis_Config {
+pub struct RedisConfig 
+{
     pub conn_str: String,
-    pub username: Optional<String>,
-    pub password: Optional<String>
+    pub username: Option<String>,
+    pub password: Option<String>
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct BS_Client {
+pub struct BsClient
+{
     pub conn_str: String,
     pub zmq_topic: String,
     pub redis_channel: String
