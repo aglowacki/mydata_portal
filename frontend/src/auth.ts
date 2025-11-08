fd718d3c8b6f1793c230639178e7baecbfc7dae8
Arthur Glowacki <aglowakci@anl.gov>
2025-11-08 11:16:51 -0600

    Refactored auth to be class

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -60,98 +60,77 @@ export async function check_user(): Promise<Response>
 
 }
 
-async function login_call(user: User): Promise<LoginResponse> 
+export class LoginFormApp
 {
 
-    const headers = new Headers({
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      });
-    
-      // Create the requestOptions object
-      const requestOptions: RequestInit = {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(user),
-      };
+    private main_div: HTMLDivElement;
+    private login_form: HTMLFormElement;
+    private username_input: HTMLInputElement;
+    private password_input: HTMLInputElement;
+    private submit_button: HTMLButtonElement;
 
-    try 
+    constructor() 
     {
-        const response = await fetch('/api/authorize', requestOptions);
-        if (!response.ok) 
-        {
-            if( response.status == 502)
-            {
-                throw new Error(`Backend Auth Serivce unreachable. May be offline: ${response.status}`);
-            }
-            else if ( response.status == 401)
-            {
-                throw new Error(`Bad login credentials. Please Try again. ${response.status}`);
-            }
-            else
-            {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-        }
-        else
-        {
-            const data = await response.json();
-            return data as LoginResponse;
-        }
-    }
-    catch (error) 
-    {
-        console.error('There was a problem with the fetch operation:', error);
-        throw error;
+        this.initializeElements();
+        this.setupEventListeners();
     }
 
-}
+    public gen_main_div(): HTMLDivElement
+    {
+        return this.main_div;
+    }
 
-function gen_signin_form()
-{
-    const form = document.createElement("form");
-    //form.classList.add("nav");
-    //form.classList.add("right");
-    form.setAttribute('id', 'loginForm');
-    form.classList.add("login_center");
-    
-    const logo_img = document.createElement('img');
-    logo_img.src ='https://login.anl.gov/idp/images/argonne_logo.png';
-    logo_img.width = 167;
-    logo_img.height = 67;
-    logo_img.classList.add("login_center");
+    private initializeElements(): void 
+    {
+        this.login_form = document.createElement("form") as HTMLFormElement;
+        this.login_form.setAttribute('id', 'loginForm');
+        this.login_form.classList.add("login_center");
+        
+        const logo_img = document.createElement('img');
+        logo_img.src ='https://login.anl.gov/idp/images/argonne_logo.png';
+        logo_img.width = 167;
+        logo_img.height = 67;
+        logo_img.classList.add("login_center");
 
-    const username_input = document.createElement("input");
-    username_input.required = true;
-    username_input.id = 'username';
-    username_input.placeholder = 'Argonne Username';
-    username_input.classList.add("login_center");
+        this.username_input = document.createElement("input") as HTMLInputElement;
+        this.username_input.required = true;
+        this.username_input.id = 'username';
+        this.username_input.placeholder = 'Argonne Username';
+        this.username_input.classList.add("login_center");
 
-    const password_input = document.createElement("input");
-    password_input.type = 'password';
-    password_input.required = true;
-    password_input.id = 'password';
-    password_input.placeholder = 'Password';
-    password_input.classList.add("login_center");
+        this.password_input = document.createElement("input") as HTMLInputElement;
+        this.password_input.type = 'password';
+        this.password_input.required = true;
+        this.password_input.id = 'password';
+        this.password_input.placeholder = 'Password';
+        this.password_input.classList.add("login_center");
 
-    const submit_button = document.createElement('button');
-    submit_button.textContent = 'Login';
-    submit_button.type = 'submit';
-    submit_button.classList.add("login_center");
-    
-    form.appendChild(logo_img);
-    form.appendChild(document.createElement('br'));
-    form.appendChild(username_input);
-    form.appendChild(document.createElement('br'));
-    form.appendChild(password_input);
-    form.appendChild(document.createElement('br'));
-    form.appendChild(submit_button);
+        this.submit_button = document.createElement('button') as HTMLButtonElement;
+        this.submit_button.textContent = 'Login';
+        this.submit_button.type = 'submit';
+        this.submit_button.classList.add("login_center");
+        
+        this.login_form.appendChild(logo_img);
+        this.login_form.appendChild(document.createElement('br'));
+        this.login_form.appendChild(this.username_input);
+        this.login_form.appendChild(document.createElement('br'));
+        this.login_form.appendChild(this.password_input);
+        this.login_form.appendChild(document.createElement('br'));
+        this.login_form.appendChild(this.submit_button);
 
-    form.addEventListener('submit', (event) => 
+        this.main_div.appendChild(this.login_form);
+    }
+
+    private setupEventListeners(): void
+    {
+        this.login_form.addEventListener('submit', (event) => this.handleLoginSubmit(event));
+    }
+
+    private handleLoginSubmit(event: Event)
     {
         event.preventDefault();
-        const user: User = { client_id: username_input.value, client_secret: password_input.value };
-        const resp = login_call(user);
+        const user: User = { client_id: this.username_input.value, client_secret: this.password_input.value };
+        const resp = this.login_call(user);
         resp.then(lres =>
         {
             set_cookie('access_token', lres.token_type + " " +lres.access_token);
@@ -163,16 +142,52 @@ function gen_signin_form()
             throw error;
         }
         );
-    });
+    }
 
-    return form;
-}
+    private async login_call(user: User): Promise<LoginResponse> 
+    {
 
-export function gen_login_form()
-{
-    const main_div = document.createElement("div");
-    main_div.appendChild(gen_signin_form());
-    //main_div.appendChild(gen_anim());
+        const headers = new Headers({
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        });
+        
+        // Create the requestOptions object
+        const requestOptions: RequestInit = {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(user),
+        };
 
-    return main_div;
-}
+        try 
+        {
+            const response = await fetch('/api/authorize', requestOptions);
+            if (!response.ok) 
+            {
+                if( response.status == 502)
+                {
+                    throw new Error(`Backend Auth Serivce unreachable. May be offline: ${response.status}`);
+                }
+                else if ( response.status == 401)
+                {
+                    throw new Error(`Bad login credentials. Please Try again. ${response.status}`);
+                }
+                else
+                {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+            }
+            else
+            {
+                const data = await response.json();
+                return data as LoginResponse;
+            }
+        }
+        catch (error) 
+        {
+            console.error('There was a problem with the fetch operation:', error);
+            throw error;
+        }
+    }
+
+};
