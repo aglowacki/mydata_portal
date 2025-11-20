e89689f2a5cbbd7c03b6e136271d9a2b071a7f6a
aglowacki <aglowacki.anl@gmail.com>
2025-11-20 21:19:39 +0000

    Added class to get beamline scans and logs

--- frontend/src/beamline.ts
+++ frontend/src/beamline.ts
@@ -1,15 +1,60 @@
     import { gen_index } from './general-helper';
     
-    interface Beamline_Log {
-      time: number;
-      msg: string;
+    interface Beamline_Log 
+    {
+        time: number;
+        msg: string;
     }
+
+    type Beamline_Logs = Beamline_Log[];
+
+
+    interface ParameterKind 
+    {
+        name: string;
+        value: number;
+    }
+
+    interface Parameter 
+    {
+        description: string;
+        kind: ParameterKind;
+        name: string;
+        default?: string; // Optional property
+    }
+
+    interface PlanProperties 
+    {
+        is_generator: boolean;
+    }
+
+    interface Plan 
+    {
+        description: string;
+        module: string;
+        name: string;
+        parameters: Parameter[];
+        properties: PlanProperties;
+    }
+
+    interface PlansAllowed 
+    {
+        [key: string] : Plan; // Allows for dynamic keys
+    }
+
+    interface ScanApiResponse 
+    {
+        success: boolean;
+        msg: string;
+        plans_allowed: PlansAllowed;
+    }
+
 
     class BeamlineLogWidget
     {
 
         private beamline_id: string;
-        private logs: Array<Beamline_Log> | null;
+        private logs: Beamline_Logs | null;
 
         private main_div: HTMLDivElement;
         //private main_div: HTMLDivElement;
@@ -21,7 +66,12 @@
             this.main_div.classList.add("beamline-log");
             this.logs = null;
             this.beamline_id = beam_id;
-            this.loadLogs();
+            this.fetcLogs().then(logs => 
+            {
+                //this.logs = logs
+                this.populateLogs(logs);
+            }
+            );
         }
 
         public gen_main_div(): HTMLDivElement
@@ -29,20 +79,20 @@
             return this.main_div;
         }
 
-        private async loadLogs(): Promise<void>
+        private async fetcLogs(): Promise<Beamline_Log[]>
         {
             try 
             {
                 const response = await fetch('/api/get_beamline_log/'+this.beamline_id);
-                
+
                 if (!response.ok) 
                 {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
                 
-                this.logs = await response.json();
-                this.populateLogs();
-                //this.showMessage('Sample meta data groups loaded successfully', 'success');
+                //this.logs = await response.json();
+                const json_repl: Beamline_Log[] = await response.json();
+                return json_repl;
             }
             catch (error) 
             {
@@ -51,26 +101,123 @@
             }
         }
 
-        private populateLogs(): void
+        private populateLogs(nlogs: Beamline_Logs | null): void
         {
+            //console.log(nlogs);
             this.main_div.innerText = "";
 
-
-            this.main_div.appendChild
             let htmlList = document.createElement("ul") as HTMLUListElement;
-            this.logs?.forEach((item) => 
+            nlogs?.forEach((item: Beamline_Log) => 
             {
                 let list_item = document.createElement("li") as HTMLLIElement;
-                list_item.innerText = item.msg;
+                list_item.textContent  = item.msg;
                 htmlList.appendChild(list_item);
             });
             this.main_div.appendChild(htmlList);
         }
     }
 
+
+    class BeamlineScansWidget
+    {
+        private beamline_id: string;
+        private main_div: HTMLDivElement;
+        private available_scans: ScanApiResponse | null;
+
+        constructor(beam_id: string)
+        {
+            this.main_div = document.createElement("div") as HTMLDivElement;
+            this.main_div.id = "beamline-scans";
+            this.main_div.classList.add("beamline-scans");
+            this.available_scans = null;
+            this.beamline_id = beam_id;
+            this.fetchAvailableScans().then(scans => 
+            {
+                //this.logs = logs
+                this.populateScans(scans);
+            }
+            );
+        }
+
+        public gen_main_div(): HTMLDivElement
+        {
+            return this.main_div;
+        }
+
+        private async fetchAvailableScans(): Promise<ScanApiResponse>
+        {
+            try 
+            {
+                const response = await fetch('/api/get_available_scans/'+this.beamline_id);
+
+                if (!response.ok) 
+                {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                
+                //this.logs = await response.json();
+                const json_repl: ScanApiResponse = await response.json();
+                return json_repl;
+            }
+            catch (error) 
+            {
+                console.error('Error loading sample types:', error);
+                //this.showMessage('Failed to load sample types. Using default values.', 'error');
+            }
+        }
+
+        private populateScans(scans: ScanApiResponse | null): void
+        {
+            console.log(scans);
+            this.main_div.innerText = "";
+
+            if (scans?.success === true)
+            {
+                let htmlList = document.createElement("ul") as HTMLUListElement;
+                Object.keys(scans.plans_allowed).forEach(key => 
+                {
+                    //console.log(key, scans.plans_allowed[key]);
+                    let list_item = document.createElement("li") as HTMLLIElement;
+                    list_item.textContent  = scans.plans_allowed[key].name;
+                    htmlList.appendChild(list_item);
+                });
+                
+                this.main_div.appendChild(htmlList);
+            }
+        }
+        
+    }
+
+    class BeamlineWidget
+    {
+        private scans_widget: BeamlineScansWidget;
+        private logs_widget: BeamlineLogWidget;
+        private main_div: HTMLDivElement;
+        private beamline_id: string;
+
+        constructor(beam_id: string)
+        {
+            this.main_div = document.createElement("div") as HTMLDivElement;
+            this.main_div.id = "beamline-widget";
+            this.main_div.classList.add("beamline-widget");
+            this.scans_widget = new BeamlineScansWidget(beam_id);
+            this.logs_widget = new BeamlineLogWidget(beam_id);
+            this.beamline_id = beam_id;
+            this.main_div.appendChild(this.scans_widget.gen_main_div())
+            this.main_div.appendChild(this.logs_widget.gen_main_div())
+        }
+        
+        public gen_main_div(): HTMLDivElement
+        {
+            return this.main_div;
+        }
+
+    }
+
 // Initialize the application when the DOM is loaded
 document.addEventListener('DOMContentLoaded', () => 
 {
-    let sapp = new BeamlineLogWidget('sec0');
-    gen_index('app', sapp.gen_main_div());
+    let beamline_id = 'sec0';
+    let blw = new BeamlineWidget(beamline_id);
+    gen_index('app', blw.gen_main_div());
 });
