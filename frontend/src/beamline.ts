df8c967f6ed0f0c5777ccd06f1e4a337a614fd1e
aglowacki <aglowacki.anl@gmail.com>
2025-12-02 01:11:16 +0000

    Added tasks view to beamline. Added tabs. Added refresh button

--- frontend/src/beamline.ts
+++ frontend/src/beamline.ts
@@ -1,12 +1,13 @@
     import { gen_index } from './general-helper';
+    import { get_cookie, has_cookie, set_cookie } from "./cookies";
     
-    interface Beamline_Log 
+    interface BeamlineLog 
     {
         time: number;
         msg: string;
     }
 
-    type Beamline_Logs = Array<Beamline_Log>;
+    type BeamlineLogs = Array<BeamlineLog>;
 
 
     interface ParameterKind 
@@ -49,27 +50,111 @@
         plans_allowed: PlansAllowed;
     }
 
+    interface CommandArgs
+    {
+         [key: string]: string
+    }
+    interface BeamlineTask
+    {
+        beamline_id: string,
+        status: string,
+        cmd: string,
+        args: CommandArgs,
+        username?: string,
+        reply?: string,
+        proc_start_time?: string, // DateTime<Utc>,
+        proc_end_time?: string, //DateTime<Utc>,
+        original_str?: string,
+    }
+    interface BeamlineTasks
+    {
+        beamline_id: string,
+        queued: Array<BeamlineTask>,
+        processing: Array<BeamlineTask>,
+        done: Array<BeamlineTask>,
+    }
 
     class BeamlineLogWidget
     {
 
         private beamline_id: string;
-        private logs: Beamline_Logs | null;
+        private logs: BeamlineLogs | null;
+        private tasks: BeamlineTask | null;
 
         private main_div: HTMLDivElement;
-        //private main_div: HTMLDivElement;
+        private tab_div: HTMLDivElement;
+        private logs_div: HTMLDivElement;
+        private tasks_div: HTMLDivElement;
+        private tab_link_logs: HTMLButtonElement;
+        private tab_link_tasks: HTMLButtonElement;
+        private beamline_event_source: EventSource;
 
         constructor(beam_id: string)
         {
             this.main_div = document.createElement("div") as HTMLDivElement;
-            this.main_div.id = "beamline-log";
-            this.main_div.classList.add("beamline-log");
+            this.main_div.id = "beamline-log-main";
+            this.main_div.classList.add("beamline-log-main");
+            this.tab_div = document.createElement("div") as HTMLDivElement;
+            this.tab_div.id = "beamline-tab";
+            this.tab_div.classList.add("tab");
+            
+            this.logs_div = document.createElement("div") as HTMLDivElement;
+            this.logs_div.id = "beamline-logs";
+            this.logs_div.classList.add("beamline-log");
+            this.tasks_div = document.createElement("div") as HTMLDivElement;
+            this.tasks_div.id = "beamline-tasks";
+            this.tasks_div.classList.add("beamline-log");
+            this.tasks_div.classList.add("hidden");
             this.logs = null;
-            this.beamline_id = beam_id;
+            this.tasks = null;
+            this.beamline_id = beam_id; 
+
+            this.tab_link_logs = document.createElement('button') as HTMLButtonElement;
+            this.tab_link_logs.innerText = "Logs";
+            this.tab_link_logs.classList.add('beamline-log-tab');
+            this.tab_link_logs.addEventListener('click', (event)=> { this.setlogsVisible(); });
+            this.tab_link_tasks = document.createElement('button') as HTMLButtonElement;
+            this.tab_link_tasks.innerText = "Tasks";
+            this.tab_link_tasks.classList.add('beamline-log-tab');
+            this.tab_link_tasks.addEventListener('click', (event)=> { this.setTasksVisible(); });
+
+
+            this.tab_div.appendChild(this.tab_link_logs);
+            this.tab_div.appendChild(this.tab_link_tasks);
+
+            this.main_div.appendChild(this.tab_div);
+            this.main_div.appendChild(this.logs_div);
+            this.main_div.appendChild(this.tasks_div);
+
+            
+            this.beamline_event_source = new EventSource("api/sse", { withCredentials: true} );
+            this.beamline_event_source.onopen = (event) => 
+            {
+                console.log('SSE connection established.', event);
+            };
+            
+            this.beamline_event_source.onmessage = (event) => 
+            {
+                console.log("Received SSE message:", event.data);
+            };
+
+            this.beamline_event_source.onerror = (error) => 
+            {
+                console.error("SSE error:", error);
+                this.beamline_event_source.close();
+            };
+            
             this.fetcLogs().then(logs => 
             {
                 //this.logs = logs
                 this.populateLogs(logs);
+            }
+            );
+
+            this.fetcTasks().then(Tasks => 
+            {
+                //this.tasks = Tasks
+                this.populateTasks(Tasks);
             }
             );
         }
@@ -79,7 +164,19 @@
             return this.main_div;
         }
 
-        private async fetcLogs(): Promise<Beamline_Logs | null>
+        private setlogsVisible(): void
+        {
+            this.logs_div.classList.remove('hidden')
+            this.tasks_div.classList.add('hidden')
+        }
+
+        private setTasksVisible(): void
+        {
+            this.logs_div.classList.add('hidden')
+            this.tasks_div.classList.remove('hidden')
+        }
+
+        private async fetcLogs(): Promise<BeamlineLogs | null>
         {
             try 
             {
@@ -91,7 +188,7 @@
                 }
                 
                 //this.logs = await response.json();
-                const json_repl: Beamline_Logs = await response.json();
+                const json_repl: BeamlineLogs = await response.json();
                 return json_repl;
             }
             catch (error) 
@@ -102,28 +199,143 @@
             }
         }
 
-        private populateLogs(nlogs: Beamline_Logs | null): void
+        private async fetcTasks(): Promise<BeamlineTasks | null>
+        {
+            try 
+            {
+                const response = await fetch('/api/get_beamline_worker_task_queues/'+this.beamline_id);
+
+                if (!response.ok) 
+                {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                
+                //this.logs = await response.json();
+                const json_repl: BeamlineTasks = await response.json();
+                return json_repl;
+            }
+            catch (error) 
+            {
+                console.error('Error loading sample types:', error);
+                return null;
+                //this.showMessage('Failed to load sample types. Using default values.', 'error');
+            }
+        }
+
+        private populateLogs(nlogs: BeamlineLogs | null): void
         {
             //console.log(nlogs);
-            this.main_div.innerText = "";
+            this.logs_div.innerText = "";
 
             //nlogs?.reverse();
             let htmlList = document.createElement("ul") as HTMLUListElement;
-            nlogs?.forEach((item: Beamline_Log) => 
+            nlogs?.forEach((item: BeamlineLog) => 
             {
                 let list_item = document.createElement("li") as HTMLLIElement;
                 list_item.textContent  = item.msg;
                 htmlList.appendChild(list_item);
             });
-            this.main_div.appendChild(htmlList);
+            this.logs_div.appendChild(htmlList);
+        }
+
+        private populateTasks(bTasks: BeamlineTasks | null): void
+        {
+            //console.log(nlogs);
+            this.tasks_div.innerText = "";
+
+            //nlogs?.reverse();
+            let table = document.createElement("table") as HTMLTableElement;
+            let hrow = table.insertRow();
+            let th0 = document.createElement("th");
+            th0.innerText = "Status";
+            hrow.appendChild(th0);
+            let th1 = document.createElement("th");
+            th1.innerText = "Command";
+            hrow.appendChild(th1);
+            let th2 = document.createElement("th");
+            th2.innerText = "Username";
+            hrow.appendChild(th2);
+            let th3 = document.createElement("th");
+            th3.innerText = "Reply";
+            hrow.appendChild(th3);
+            let th4 = document.createElement("th");
+            th4.innerText = "Start Time";
+            hrow.appendChild(th4);
+            let th5 = document.createElement("th");
+            th5.innerText = "End Time";
+            hrow.appendChild(th5);
+            bTasks?.queued.forEach((item: BeamlineTask) => 
+            {
+                let row = table.insertRow();
+                let cell0 = row.insertCell();
+                cell0.innerText = item.status;
+                let cell1 = row.insertCell();
+                cell1.innerText = item.cmd;
+                let cell2 = row.insertCell();
+                cell2.innerText = item.username!;
+                let cell3 = row.insertCell();
+                let div = document.createElement('div') as HTMLDivElement;
+                div.innerText = item.reply!;
+                div.classList.add('scrollable');
+                cell3.appendChild(div);
+                let cell4 = row.insertCell();
+                cell4.innerText = item.proc_start_time!;
+                let cell5 = row.insertCell();
+                cell5.innerText = item.proc_end_time!;
+                
+            });
+            bTasks?.processing.forEach((item: BeamlineTask) => 
+            {
+                let row = table.insertRow();
+                let cell0 = row.insertCell();
+                cell0.innerText = item.status;
+                let cell1 = row.insertCell();
+                cell1.innerText = item.cmd;
+                let cell2 = row.insertCell();
+                cell2.innerText = item.username!;
+                let cell3 = row.insertCell();
+                let div = document.createElement('div') as HTMLDivElement;
+                div.innerText = item.reply!;
+                div.classList.add('scrollable');
+                cell3.appendChild(div);
+                let cell4 = row.insertCell();
+                cell4.innerText = item.proc_start_time!;
+                let cell5 = row.insertCell();
+                cell5.innerText = item.proc_end_time!;
+                
+            });
+            bTasks?.done.forEach((item: BeamlineTask) => 
+            {
+                let row = table.insertRow();
+                let cell0 = row.insertCell();
+                cell0.innerText = item.status;
+                let cell1 = row.insertCell();
+                cell1.innerText = item.cmd;
+                let cell2 = row.insertCell();
+                cell2.innerText = item.username!;
+                let cell3 = row.insertCell();
+                let div = document.createElement('div') as HTMLDivElement;
+                div.innerText = item.reply!;
+                div.classList.add('scrollable');
+                cell3.appendChild(div);
+                let cell4 = row.insertCell();
+                cell4.innerText = item.proc_start_time!;
+                let cell5 = row.insertCell();
+                cell5.innerText = item.proc_end_time!;
+                
+            });
+            this.tasks_div.appendChild(table);
         }
     }
-
-
+ 
     class BeamlineScansWidget
     {
         private beamline_id: string;
         private main_div: HTMLDivElement;
+        private queued_div: HTMLDivElement;
+        private processing_div: HTMLDivElement;
+        private done_div: HTMLDivElement;
+        
         private available_scans: ScanApiResponse | null;
 
         constructor(beam_id: string)
@@ -131,8 +343,27 @@
             this.main_div = document.createElement("div") as HTMLDivElement;
             this.main_div.id = "beamline-scans";
             this.main_div.classList.add("beamline-scans");
+
+            this.queued_div = document.createElement("div") as HTMLDivElement;
+            this.queued_div.id = "beamline-scans-queued";
+            this.queued_div.classList.add("beamline-scans");
+            
+            this.processing_div = document.createElement("div") as HTMLDivElement;
+            this.processing_div.id = "beamline-scans-processing";
+            this.processing_div.classList.add("beamline-scans");
+            
+            this.done_div = document.createElement("div") as HTMLDivElement;
+            this.done_div.id = "beamline-scans-done";
+            this.done_div.classList.add("beamline-scans");
+            
+
+            this.main_div.appendChild(this.queued_div);
+            this.main_div.appendChild(this.processing_div);
+            this.main_div.appendChild(this.done_div);
+
             this.available_scans = null;
             this.beamline_id = beam_id;
+
             this.fetchAvailableScans().then(scans => 
             {
                 this.available_scans = scans;
@@ -168,14 +399,59 @@
                 return null;
             }
         }
+/*
+        private async fetchScanQueue(): Promise<ScanApiResponse | null>
+        {
+            try 
+            {
+                const response = await fetch('/api/get_available_scans/'+this.beamline_id);
 
+                if (!response.ok) 
+                {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                
+                //this.logs = await response.json();
+                const json_repl: ScanApiResponse = await response.json();
+                return json_repl;
+            }
+            catch (error) 
+            {
+                console.error('Error loading sample types:', error);
+                //this.showMessage('Failed to load sample types. Using default values.', 'error');
+                return null;
+            }
+        }
+*/
         private populateScans(scans: ScanApiResponse | null): void
         {
             console.log(scans);
-            this.main_div.innerText = "";
+            this.queued_div.innerText = "";
 
             if (scans?.success === true)
             {
+                //nlogs?.reverse();
+                let table = document.createElement("table") as HTMLTableElement;
+                let hrow = table.insertRow();
+                let th0 = document.createElement("th");
+                th0.innerText = "Status";
+                hrow.appendChild(th0);
+                let th1 = document.createElement("th");
+                th1.innerText = "Command";
+                hrow.appendChild(th1);
+                let th2 = document.createElement("th");
+                th2.innerText = "Username";
+                hrow.appendChild(th2);
+                let th3 = document.createElement("th");
+                th3.innerText = "Reply";
+                hrow.appendChild(th3);
+                let th4 = document.createElement("th");
+                th4.innerText = "Start Time";
+                hrow.appendChild(th4);
+                let th5 = document.createElement("th");
+                th5.innerText = "End Time";
+                hrow.appendChild(th5);
+
                 let htmlList = document.createElement("ul") as HTMLUListElement;
                 Object.keys(scans.plans_allowed).forEach(key => 
                 {
@@ -194,6 +470,7 @@
     class BeamlineWidget
     {
         private scans_widget: BeamlineScansWidget;
+        private refresh_plans_btn: HTMLButtonElement;
         private logs_widget: BeamlineLogWidget;
         private main_div: HTMLDivElement;
         private beamline_id: string;
@@ -206,8 +483,15 @@
             this.scans_widget = new BeamlineScansWidget(beam_id);
             this.logs_widget = new BeamlineLogWidget(beam_id);
             this.beamline_id = beam_id;
+            this.refresh_plans_btn = document.createElement("button") as HTMLButtonElement;
+            this.refresh_plans_btn.innerText="Refresh Scan Plans";
+            this.refresh_plans_btn.addEventListener('click', (event)=>
+            { this.refresh_plans(); });
+
+            this.main_div.appendChild(this.refresh_plans_btn);
             this.main_div.appendChild(this.scans_widget.gen_main_div())
             this.main_div.appendChild(this.logs_widget.gen_main_div())
+            
         }
         
         public gen_main_div(): HTMLDivElement
@@ -215,6 +499,43 @@
             return this.main_div;
         }
 
+        private async refresh_plans(): Promise<void>
+        {
+            try 
+            {
+                const auth_cookie:string = get_cookie('access_token');
+
+                const headers = new Headers({
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': auth_cookie,
+                });
+                let cargs: CommandArgs = {
+                    user_group: 'primary'
+                }
+                let command: BeamlineTask = {
+                    status: " ",
+                    beamline_id: this.beamline_id,
+                    cmd: "plans_allowed",
+                    args: cargs,
+                }
+                console.log(command);
+                const requestOptions: RequestInit = {
+                    method: 'POST',
+                    headers: headers,
+                    body: JSON.stringify(command),
+                };
+
+                
+                const response = await fetch('/api/queue_beamline_worker_task/'+this.beamline_id, requestOptions);
+                const data = await response;
+                console.log(data);
+            }
+            catch (error) 
+            {
+                console.error('Error refreshing plans:', error);
+            }
+        }
     }
 
 // Initialize the application when the DOM is loaded
