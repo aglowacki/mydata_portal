7f3e37d7c165be0748713edc637283956b89d047
Arthur Glowacki <aglowacki@hyrule.xray.aps.anl.gov>
2026-07-30 11:10:54 -0500

    changed fixations

--- database/sql/101_insert_bio_sample_fixations.sql
+++ database/sql/101_insert_bio_sample_fixations.sql
@@ -2,6 +2,13 @@ INSERT INTO bio_sample_fixations (name, fixative_id) VALUES
 ('Chemically Fixed', (SELECT id FROM bio_sample_fixatives WHERE name = 'Formaldehyde')),
 ('Chemically Fixed', (SELECT id FROM bio_sample_fixatives WHERE name = 'Glutaraldehyde')),
 ('Chemically Fixed', (SELECT id FROM bio_sample_fixatives WHERE name = 'Farmaldehyde-Glutaraldehyde Mixtures')),
-('Frozen Hydrated', (SELECT id FROM bio_sample_fixatives WHERE name = 'None')),
-('None', (SELECT id FROM bio_sample_fixatives WHERE name = 'None')),
-('Dehydrated', (SELECT id FROM bio_sample_fixatives WHERE name = 'None'));
+('Chemically Fixed', (SELECT id FROM bio_sample_fixatives WHERE name = 'None')),
+('Cryofixation', (SELECT id FROM bio_sample_fixatives WHERE name = 'Formaldehyde')),
+('Cryofixation', (SELECT id FROM bio_sample_fixatives WHERE name = 'Glutaraldehyde')),
+('Cryofixation', (SELECT id FROM bio_sample_fixatives WHERE name = 'Farmaldehyde-Glutaraldehyde Mixtures')),
+('Cryofixation', (SELECT id FROM bio_sample_fixatives WHERE name = 'None')),
+('Chemically and Cryofixation', (SELECT id FROM bio_sample_fixatives WHERE name = 'Formaldehyde')),
+('Chemically and Cryofixation', (SELECT id FROM bio_sample_fixatives WHERE name = 'Glutaraldehyde')),
+('Chemically and Cryofixation', (SELECT id FROM bio_sample_fixatives WHERE name = 'Farmaldehyde-Glutaraldehyde Mixtures')),
+('Chemically and Cryofixation', (SELECT id FROM bio_sample_fixatives WHERE name = 'None')),
+('None', (SELECT id FROM bio_sample_fixatives WHERE name = 'None'));
